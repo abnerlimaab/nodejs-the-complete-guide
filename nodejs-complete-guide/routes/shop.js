a63385e1f2e5de0cd96715383744d3a2928ed3f2
Abner Lima <abnerlima@pmenos.com.br>
2023-05-26 20:40:35 -0300

    outputting dynamic content

--- nodejs-complete-guide/routes/shop.js
+++ nodejs-complete-guide/routes/shop.js
@@ -1,14 +1,16 @@
-const path = require("path");
-
 const express = require("express");
 
-const rootDir = require("../util/path");
 const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("shop");
+  const { products } = adminData;
+
+  res.render("shop", {
+    prods: products,
+    docTitle: "Shop",
+  });
 });
 
 module.exports = router;
