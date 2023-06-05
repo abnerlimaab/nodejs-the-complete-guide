0aae2c956ebfc7b963c77618c1d9ca0b4c15427f
Abner Lima <abnerlima@pmenos.com.br>
2023-06-05 20:05:55 -0300

    adding controllers

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -1,30 +1,27 @@
-const http = require("http");
 const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", {
-    pageTitle: "Page Not Found",
-    path: "",
-  });
+app.use(errorController.get404Page);
+
+app.listen(3000, () => {
+  console.info("Server listening on port 3000");
+  console.info("http://localhost:3000");
 });
-
-const server = http.createServer(app);
-
-server.listen(3000);
--- nodejs-complete-guide/routes/shop.js
+++ nodejs-complete-guide/routes/shop.js
@@ -1,17 +1,9 @@
 const express = require("express");
 
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const { products } = adminData;
-
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
