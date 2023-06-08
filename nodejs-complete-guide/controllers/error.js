3845710103436b6d15d8b26f22c32c4bbe071f3e
Abner Lima <abnerlima@pmenos.com.br>
2023-06-08 20:53:53 -0300

    creating and registering routes

--- nodejs-complete-guide/controllers/error.js
+++ nodejs-complete-guide/controllers/error.js
@@ -1,6 +1,6 @@
 exports.get404Page = (_, res) => {
   res.status(404).render("404", {
     pageTitle: "Page Not Found",
-    path: "",
+    path: "/404",
   });
 };
---
+++ nodejs-complete-guide/controllers/shop.js
@@ -0,0 +1,35 @@
+const Product = require("../models/product");
+
+exports.getProducts = (_, res) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getIndex = (_, res) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (_, res) => {
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart",
+  });
+};
+
+exports.getCheckout = (_, res) => {
+  res.render("shop/checkout", {
+    pageTitle: "Checkout",
+    path: "/checkout",
+  });
+};
