849fb5d2715aeafd4b52b323435fc752419cd24b
Abner Lima <abnerlima.1997@gmail.com>
2023-07-26 23:56:01 -0300

    working on cart items and orders

--- nodejs-complete-guide/models/user.js
+++ nodejs-complete-guide/models/user.js
@@ -2,9 +2,11 @@ const mongodb = require("mongodb");
 const { getDb } = require("../util/database");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -12,6 +14,20 @@ class User {
     return db
       .collection("users")
       .insertOne(this)
+  }
+
+  addToCart(product) {
+    // const cartProductIndex = this.cart.items.findIndex((cp) => cp._id.toString() === product._id.toString());
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+
+    const db = getDb();
+
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
